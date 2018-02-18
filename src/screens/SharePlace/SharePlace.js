57dc7a49754376b413f64bf67d27684fe8516db8
mvizm2015 <mariaviz2015@gmail.com>
2018-02-18 15:16:49 -0500

    React-Native-Navigation

---
+++ src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class SharePlaceScreen extends Component {
+    render () {
+        return (
+            <View>
+                <Text>On Share PlaceScreen </Text>
+            </View>
+        ); 
+    }
+}
+
+export default SharePlaceScreen;
