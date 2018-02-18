be34cc834091aac483c7ee064766e04b157ec70f
mvizm2015 <mariaviz2015@gmail.com>
2018-02-18 00:20:37 -0500

    External libraries - icons

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import placeReducer from './reducers/places';
 
@@ -7,8 +7,14 @@ const rootReducer = combineReducers({
     places: placeReducer
 });
 
+let composeEnhancers = compose;
+
+if(__DEV__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-    return createStore(rootReducer);
+    return createStore(rootReducer, composeEnhancers());
 };
 
 export default configureStore;
